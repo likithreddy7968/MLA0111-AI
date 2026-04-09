b168b0f52d36c10b43d7c59e2162aab573dec505
likithreddy7968 <192425134.simats@saveetha.com>
2026-04-09 10:44:41 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,21 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    if start not in visited:
+        print(start, end=" ")
+        visited.add(start)
+        for neighbor in graph[start]:
+            dfs(graph, neighbor, visited)
+
+# Example graph as adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+dfs(graph, 'A')
